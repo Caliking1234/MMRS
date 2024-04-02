f7730462950b37ce4e019d05c11b0f42f12e4b32
Piyush <piyyush1219@gmail.com>
2024-04-02 11:53:15 +0530

    ..

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroAnimation from "@/components/HeroAnimation";
+import TextAnimation from "@/components/TextAnimation";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen bg-gray-300 flex-col">
       <HeroAnimation />
+      <TextAnimation />
     </main>
   );
 }
